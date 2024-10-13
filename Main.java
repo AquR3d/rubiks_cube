9afaed128264719a95229fd0db84edf7a562319e
AquR3d <123186046+AquR3d@users.noreply.github.com>
2024-10-13 14:17:02 +0000

    fixed @param Cube other constructor

--- Main.java
+++ Main.java
@@ -4,10 +4,14 @@ public class Main{
     public static void main(String[] args){
 
         Cube cube = new Cube();
+        Cube cube2 = cube;
+        Cube cube3 = new Cube(cube);
 
         cube.turn("W'");
 
         System.out.println(cube);
+        System.out.println(cube2);
+        System.out.println(cube3);
 
         /*
         // showcase all 18 permutations
